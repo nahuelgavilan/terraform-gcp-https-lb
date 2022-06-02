1188d0285164e03d02419f8594a40fec2d682a8e
Nahuel Gavilán <nahuelgavilanbe@gmail.com>
2022-06-02 11:16:34 +0200

    First Commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.6.0"
+    }
+  }
+}
+
+provider "google" {
+  region      = var.region
+  zone        = var.zone
+  project     = var.project_id
+}
